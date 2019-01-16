1c82a164e5345cd21a02eda128caf3d0ce9a3e3e
Mathias Beguin <mathias.beguin@hotmail.com>
2019-01-16 20:00:13 +0100

    Saving / Loading policy done

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -1,9 +1,12 @@
+import torch
 import torch.nn as nn
+import datetime
 
 
 class Agent(nn.Module):
     def __init__(self):
         super(Agent, self).__init__()
+        self.models_path = "models"
 
     def forward(self, x):
         raise NotImplementedError
@@ -13,3 +16,11 @@ class Agent(nn.Module):
 
     def step(self, observations, actions, rewards, masks, last_observation):
         raise NotImplementedError
+
+    def save(self):
+        filename = self.__class__.__name__ + datetime.datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
+        torch.save(self.state_dict(), "{}/{}".format(self.models_path, filename))
+
+    def load(self, filename):
+        path = "{}/{}".format(self.models_path, filename)
+        self.load_state_dict(torch.load(path))
