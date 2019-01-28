c1b427e99989f4ed03e5e65d2a1c1e8ddfcc1100
Mathias Beguin <mathias.beguin@hotmail.com>
2019-01-28 19:51:34 +0100

    Reworked custom cartpole env as a subclass of the original cartpole

--- src/environments/cartpole_custom.py
+++ src/environments/cartpole_custom.py
@@ -1,241 +1,40 @@
-"""
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
-"""
-
-import math
-import gym
-from gym import spaces, logger
-from gym.utils import seeding
 import numpy as np
 
+from gym.envs.classic_control.cartpole import CartPoleEnv
 
-class CartPoleCustomEnv(gym.Env):
-    """
-    Description:
-        A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's velocity.
 
-    Source:
-        This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson
-
-    Observation:
-        Type: Box(4)
-        Num	Observation                 Min         Max
-        0	Cart Position             -4.8            4.8
-        1	Cart Velocity             -Inf            Inf
-        2	Pole Angle                 -24°           24°
-        3	Pole Velocity At Tip      -Inf            Inf
-
-    Actions:
-        Type: Discrete(2)
-        Num	Action
-        0	Push cart to the left
-        1	Push cart to the right
-
-        Note: The amount the velocity is reduced or increased is not fixed as it depends on the angle the pole is pointing. This is because the center of gravity of the pole increases the amount of energy needed to move the cart underneath it
-
-    Reward:
-        Reward is 1 for every step taken, including the termination step
-
-    Starting State:
-        All observations are assigned a uniform random value between ±0.05
-
-    Episode Termination:
-        Pole Angle is more than ±12°
-        Cart Position is more than ±2.4 (center of the cart reaches the edge of the display)
-        Episode length is greater than 200
-        Solved Requirements
-        Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
-    """
-
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second': 50
-    }
-
-    def __init__(self, distributions, true_value):
-        self.distributions = distributions
+class CartPoleCustomEnv(CartPoleEnv):
+    def __init__(self, gravity_bounds=None, length_bounds=None, true_value=True):
+        super().__init__()
+        self.gravity_bounds = gravity_bounds
+        self.length_bounds = length_bounds
         self.true_value = true_value
-        self.fake_gravity = 0
-        self.fake_length = 0
-
-        self.gravity = 9.8
-        self.masscart = 1.0
-        self.masspole = 0.1
-        self.total_mass = (self.masspole + self.masscart)
-        self.length = 0.5  # actually half the pole's length
-        self.polemass_length = (self.masspole * self.length)
-        self.force_mag = 10.0
-        self.tau = 0.02  # seconds between state updates
-        self.kinematics_integrator = 'euler'
-
-        # Angle at which to fail the episode
-        self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 2.4
-
-        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
-        low = np.array([
-            -self.x_threshold * 2,
-            np.finfo(np.float32).min,
-            -self.theta_threshold_radians * 2,
-            np.finfo(np.float32).min,
-            self.distributions["length"][0],
-            self.distributions["gravity"][0]
-        ])
-
-        high = np.array([
-            self.x_threshold * 2,
-            np.finfo(np.float32).max,
-            self.theta_threshold_radians * 2,
-            np.finfo(np.float32).max,
-            self.distributions["length"][1],
-            self.distributions["gravity"][1]
-        ])
-
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(low, high, dtype=np.float32)
-
-        self.seed()
-        self.viewer = None
-        self.state = None
-
-        self.steps_beyond_done = None
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+        self.parameters = []
 
     def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
-        state = self.state
-        x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action == 1 else -self.force_mag
-        costheta = math.cos(theta)
-        sintheta = math.sin(theta)
-        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta * temp) / (
-                self.length * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        if self.kinematics_integrator == 'euler':
-            x = x + self.tau * x_dot
-            x_dot = x_dot + self.tau * xacc
-            theta = theta + self.tau * theta_dot
-            theta_dot = theta_dot + self.tau * thetaacc
-        else:  # semi-implicit euler
-            x_dot = x_dot + self.tau * xacc
-            x = x + self.tau * x_dot
-            theta_dot = theta_dot + self.tau * thetaacc
-            theta = theta + self.tau * theta_dot
-        self.state = (x, x_dot, theta, theta_dot)
-        done = x < -self.x_threshold \
-               or x > self.x_threshold \
-               or theta < -self.theta_threshold_radians \
-               or theta > self.theta_threshold_radians
-        done = bool(done)
+        state, reward, done, info = super().step(action)
+        return np.append(state, self.parameters), reward, done, info
 
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warn(
-                    "You are calling 'step()' even though this environment has already returned done = True. "
-                    "You should always call 'reset()' once you receive 'done = True' "
-                    "-- any further steps are undefined behavior.")
-            self.steps_beyond_done += 1
-            reward = 0.0
+    def reset(self, gravity=None, length=None):
+        state = super().reset()
 
-        state = np.append(state, (self.length, self.gravity)) \
-            if self.true_value else np.append(state, (self.fake_length, self.fake_gravity))
+        if self.gravity_bounds:
+            self.gravity = gravity if gravity else np.random.uniform(self.gravity_bounds[0], self.gravity_bounds[1])
 
-        return state, reward, done, {}
+        if self.length_bounds:
+            self.length = length if length else np.random.uniform(self.length_bounds[0], self.length_bounds[1])
+            self.polemass_length = self.masspole * self.length
 
-    def reset(self):
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.steps_beyond_done = None
+        self.parameters = []
+        self._update_parameters()
 
-        self.length, self.gravity = self._update_nuisance()
-        self.fake_length, self.fake_gravity = self._update_nuisance()
-        self.polemass_length = (self.masspole * self.length)
+        return np.append(state, self.parameters)
 
-        state = np.append(self.state, (self.length, self.gravity)) \
-            if self.true_value else np.append(self.state, (self.fake_length, self.fake_gravity))
+    def _update_parameters(self):
+        if self.gravity_bounds:
+            self.parameters.append(self.gravity) if self.true_value \
+                else self.parameters.append(np.random.uniform(self.gravity_bounds[0], self.gravity_bounds[1]))
 
-        return state
-
-    def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
-
-        world_width = self.x_threshold * 2
-        scale = screen_width / world_width
-        carty = 100  # TOP OF CART
-        polewidth = 10.0
-        polelen = scale * (2 * self.length)
-        cartwidth = 50.0
-        cartheight = 30.0
-
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
-            axleoffset = cartheight / 4.0
-            cart = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            self.carttrans = rendering.Transform()
-            cart.add_attr(self.carttrans)
-            self.viewer.add_geom(cart)
-            l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
-            pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            pole.set_color(.8, .6, .4)
-            self.poletrans = rendering.Transform(translation=(0, axleoffset))
-            pole.add_attr(self.poletrans)
-            pole.add_attr(self.carttrans)
-            self.viewer.add_geom(pole)
-            self.axle = rendering.make_circle(polewidth / 2)
-            self.axle.add_attr(self.poletrans)
-            self.axle.add_attr(self.carttrans)
-            self.axle.set_color(.5, .5, .8)
-            self.viewer.add_geom(self.axle)
-            self.track = rendering.Line((0, carty), (screen_width, carty))
-            self.track.set_color(0, 0, 0)
-            self.viewer.add_geom(self.track)
-
-            self._pole_geom = pole
-
-        if self.state is None: return None
-
-        # Edit the pole polygon vertex
-        pole = self._pole_geom
-        l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
-        pole.v = [(l, b), (l, t), (r, t), (r, b)]
-
-        x = self.state
-        cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
-        self.carttrans.set_translation(cartx, carty)
-        self.poletrans.set_rotation(-x[2])
-
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
-
-    def _update_nuisance(self):
-        length = self.length
-        gravity = self.gravity
-
-        for key in self.distributions.keys():
-            low, high = self.distributions[key]
-
-            if key == "length":
-                length = self.np_random.uniform(low, high)
-
-            if key == "gravity":
-                gravity = self.np_random.uniform(low, high)
-
-        return length, gravity
+        if self.length_bounds:
+            self.parameters.append(self.length) if self.true_value \
+                else self.parameters.append(np.random.uniform(self.length_bounds[0], self.length_bounds[1]))
